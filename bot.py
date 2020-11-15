15730981201a2e36847203fd583b25411e664bc2
svcodes <61701181+svcodes@users.noreply.github.com>
2020-11-15 13:55:31 -0600

    music pog
    this is copy pasted all credits go to toxicrecker/discordutils https://github.com/toxicrecker/DiscordUtils

--- bot.py
+++ bot.py
@@ -6,11 +6,13 @@ import asyncpraw
 import random
 import ksoftapi
 import time
+import DiscordUtils
 
 #initialize stuff
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix = ".", intents = intents)
+music = DiscordUtils.music()
 
 reddit = asyncpraw.Reddit(client_id="CIwP1eILd2pKNA",
                      client_secret="eEMVLkXCyLD4mXC9QHjDMIWedEI",
@@ -27,6 +29,94 @@ async def gay(ctx):
        #res = await r.json()
        await ctx.send(r)
 """
+
+
+
+@client.command()
+async def join(ctx):
+    await ctx.author.voice.channel.connect() #Joins author's voice channel
+    
+@client.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+    
+@client.command()
+async def play(ctx, *, url):
+    player = music.get_player(guild_id=ctx.guild.id)
+    if not player:
+        player = music.create_player(ctx, ffmpeg_error_betterfix=True)
+    if not ctx.voice_client.is_playing():
+        await player.queue(url, search=True)
+        song = await player.play()
+        await ctx.send(f"Playing {song.name}")
+    else:
+        song = await player.queue(url, search=True)
+        await ctx.send(f"Queued {song.name}")
+        
+@client.command()
+async def pause(ctx):
+    player = music.get_player(guild_id=ctx.guild.id)
+    song = await player.pause()
+    await ctx.send(f"Paused {song.name}")
+    
+@client.command()
+async def resume(ctx):
+    player = music.get_player(guild_id=ctx.guild.id)
+    song = await player.resume()
+    await ctx.send(f"Resumed {song.name}")
+    
+@client.command()
+async def stop(ctx):
+    player = music.get_player(guild_id=ctx.guild.id)
+    await player.stop()
+    await ctx.send("Stopped")
+    
+@client.command()
+async def loop(ctx):
+    player = music.get_player(guild_id=ctx.guild.id)
+    song = await player.toggle_song_loop()
+    if song.is_looping:
+        await ctx.send(f"Enabled loop for {song.name}")
+    else:
+        await ctx.send(f"Disabled loop for {song.name}")
+    
+@client.command()
+async def queue(ctx):
+    player = music.get_player(guild_id=ctx.guild.id)
+    await ctx.send(f"{', '.join([song.name for song in player.current_queue()])}")
+    
+@client.command()
+async def np(ctx):
+    player = music.get_player(guild_id=ctx.guild.id)
+    song = player.now_playing()
+    await ctx.send(song.name)
+    
+@client.command()
+async def skip(ctx):
+    player = music.get_player(guild_id=ctx.guild.id)
+    data = await player.skip(force=True)
+    if len(data) == 2:
+        await ctx.send(f"Skipped from {data[0].name} to {data[1].name}")
+    else:
+        await ctx.send(f"Skipped {data[0].name}")
+
+@client.command()
+async def volume(ctx, vol):
+    player = music.get_player(guild_id=ctx.guild.id)
+    song, volume = await player.change_volume(float(vol / 100)) # volume should be a float between 0 to 1
+    await ctx.send(f"Changed volume for {song.name} to {volume*100}%")
+    
+@client.command()
+async def remove(ctx, index):
+    player = music.get_player(guild_id=ctx.guild.id)
+    song = await player.remove_from_queue(int(index))
+    await ctx.send(f"Removed {song.name} from queue")
+
+
+
+
+
+
 
 @commands.has_permissions(manage_messages=True)
 @client.command(help = "sets slowmode. Usage: .slowmode <seconds>")
